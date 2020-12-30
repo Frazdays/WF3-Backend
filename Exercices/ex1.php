51300e7a130d286f3a7b03fcc9d21b4ae5ce2029
sophia <lazreg.sophia@gmail.com>
2020-12-30 23:40:12 +0100

    add TP

--- Exercices/ex1.php
+++ Exercices/ex1.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
 <head>
+    <meta charset="UTF-8"/>
     <link rel="stylesheet" href="style.css"/>
+    <title>ALLO PHP - Site de formation PHP</title>
+
+    <!-- FAVICONS -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+    <link rel="manifest" href="favicons/site.webmanifest">
+    <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta name="description" content="Découvrez les joies de la programmation PHP sur ALLO PHP, meilleur site de formation PHP." />
 </head>
-
-
 
 <body>
 
 <h1>
-    Exercice de PHP : 
+    😄 Exercice de PHP&nbsp;: 
     Syntaxe de base du langage
 </h1>
 
@@ -41,22 +54,23 @@
     // echo $VITESSE_MAX; est une erreur car ce n'est PAS une VARIABLE mais une CONSTANTE
     // echo VITESSE_MAX;
     // echo " ";
-    // echo constant("VITESSE_MAX");  
+    // echo constant("VITESSE_MAX");
 ?>
 
 <?php echo "<p>".$z."</p>" ?>
 
+<p>23</p>
 
 <h2>Exemple c : incrémentation et décrémentation</h2>
 
 <?php
     // Incrémentation (ajoute 1)
-    $z++;
+    $z++; // Similaire à "++$z"
     $z = $z + 1; // Exactement le même résultat que la ligne précédente
     $z += 1; // Exactement le même résultat que la ligne précédente
 
     // Décrémentation (retire 1)
-    $z--;
+    $z--; // Similaire à "--$z"
     $z = $z - 1; // Exactement le même résultat que la ligne précédente
     $z -= 1; // Exactement le même résultat que la ligne précédente
 
@@ -66,7 +80,7 @@
 
 <p><?php echo $z ?></p>
 
-<h2>Exemple d : php dans une balise html</h2>
+<h2>Exemple d&nbsp;: php dans une balise html</h2>
 
 <ul>
     <li><?php echo $texteDuParagraphe; ?></li>
@@ -77,23 +91,24 @@
     <li><?php echo $z; ?></li>
 </ul>
 
-<h2>Exemple e : html dans une balise php </h2>
+<h2>Exemple e&nbsp;: html dans une balise php </h2>
 
 <?php echo "<p>".$texteDuParagraphe."</p>"; // "." concaténation ?>
 <?php echo "<p>"."Hello World!"."</p>"; // Exactement le même résultat que la ligne précédente ?>
 <?php echo "Hello World!"; // Exactement le même résultat que la ligne précédente ?>
 
 <?php echo "<a href='https://google.fr'>".$z."</a>" ?>
-<?php echo '<a href="https://google.fr">'.$z.'</a>' // Exactement le même résultat que la ligne précédente?>
+<?php echo '<a href="https://google.fr">'.$z.'</a>' // Exactement le même résultat que la ligne précédente ?>
 
 <?php echo 
     "<p>
         Sayah a dit : 
-        <q cite='https://google.fr'>BONJOUR L'HISTOIRE !</q>
+        <q>Bonjour l'Histoire !</q> 
+        et il a aussi ajouté : <q>Hello people !</q>
     <p>"
 ?>
 
-<h2>Exemple f : rappels html/css </h2>
+<h2>Exemple f&nbsp;: rappels html/css </h2>
 
 <p class="attention">
     Il faut nettoyer la plaie avant de mettre le 
@@ -106,8 +121,223 @@
 
 <p class="attention">
    Veillez à être CLAIR, BREF et PRECIS sur le danger et 
-   sur le lieu de l'accident
+   sur le lieu de l'accident.
 </p>
 
-</body>
+<?php print("<h2>YO !</h2>"); ?>
+
+
+<h2>Exemple g : les opérateurs php </h2>
+
+<?php 
+    $x = 20;
+    $y = 6;
+?>
+
+<ul>
+    <li><?php echo ($x + $y); // Affiche 26 ?></li>
+    <li><?php echo ($x - $y); // Affiche 14 ?></li>
+    <li><?php echo ($x * $y); // Affiche 120 ?></li>
+    <li><?php echo ($x / $y); // Affiche 3.3333333... ?></li>
+    <li><?php echo ($x % $y); // Affiche 2 ?></li>
+
+    <?php
+        $y = 20;
+        $y += 100; // similaire à "$y = $y + 100;"
+        echo "<li>".$y."</li>"; // Affiche 120
+
+        $z = 50;
+        $z -= 25;
+        echo "<li>".$z."</li>"; // Affiche 25
+
+        $i = 5;
+        $i *= 6; // similaire à "$i = $i * 6"
+        echo "<li>".$i."</li>"; // Affiche 30
+
+        $j = 10;
+        $j /= 5;
+        echo "<li>".$j."</li>"; // Affiche 2
+
+        $k = 20;
+        $k %= 7;
+        echo "<li>".$k."</li>"; // Affiche 6
+
+        $o = "Bonjour";
+        $o .= ", monde ! ";
+        echo "<li>".$o."</li>"; // Affiche "Bonjour, monde !"
+        $o .= $o; // similaire à "$o = $o . $o"
+        echo "<li>".$o."</li>"; // Affiche "Bonjour, monde ! Bonjour, monde !"
+
+        $x = 100;
+        $y = "100";
+        echo "<li>";
+            var_dump($x == $y); // "valeur égale à"
+        echo "</li>";
+
+        echo "<li>";
+            var_dump($x === $y); // "valeur et type égaux à"
+        echo "</li>";
+
+        echo "<li>";
+            var_dump($x != $y); // "valeur différente de"
+        echo "</li>";
+
+        echo "<li>";
+            var_dump($x !== $y); // "valeur et types différents de"
+        echo "</li>";
+
+        $a = 50;
+        $b = 90;
+        echo "<li>";
+            var_dump($a > $b); // "strictement supérieur à"
+        echo "</li>";
+
+        echo "<li>";
+            var_dump($a >= $b); // "supérieur ou égal à"
+        echo "</li>";
+
+        echo "<li>";
+            var_dump($a < $b); // "strictement inférieur à"
+        echo "</li>";
+
+        echo "<li>";
+        var_dump($a <= $b); // "inférieur ou égal à"
+    echo "</li>";
+    ?>
+</ul>
+
+<h2>Exemple h&nbsp;: les opérateurs logiques </h2>
+<ul>
+    <li><?php var_dump(true AND false); // ET ?></li>
+    <li><?php var_dump(true OR false); // OU ?></li>
+    <li><?php var_dump(true && false); // ET ?></li>
+    <li><?php var_dump(true || false); // OU ?></li>
+    <li><?php var_dump(!true); // NON (ici : NON VRAI, soit FAUX) ?></li>
+    <li><?php var_dump(!false); // NON (ici : NON FAUX, soit VRAI) ?></li> 
+</ul>
+
+<h2> Exemple i&nbsp;: les conditions </h2>
+
+
+<?php
+    $x = 3;
+    echo "<p>";
+    if ($x >= 10) { // 10 et +
+        echo "Bonjour&nbsp;!";
+    } elseif ($x < 5) {
+        echo "Salut&nbsp;!"; // 0, 1, 2, 3 ou 4
+    } else {
+        echo "Rien à faire."; // 5, 6, 7, 8, 9
+    }
+    
+
+    $favcolor = "red";
+    echo "<p>";
+    switch ($favcolor) {
+        case "red":
+            echo "Votre couleur préférée est le rouge&nbsp;!";
+        break;
+        case "blue":
+            echo "Votre couleur préférée est le bleu&nbsp;!";
+        break;
+        case "green":
+            echo "Votre couleur préférée est le vert&nbsp;!";
+        break;
+        default:
+            echo "Vous n’avez pas de couleur préférée&nbsp;!";
+    }
+    echo "</p>";
+
+
+    
+    $x = 1;
+
+    echo "<ul>";
+    while($x <= 5) {
+        echo "<li>$x</li>";
+        $x++;
+    }
+    echo "</ul>";
+
+    $x = 1;
+    echo "<ul>";
+    do {
+        echo "<li>$x</li>";
+        $x++;
+    } while ($x <= 5);
+
+    echo "</ul>";
+
+    echo "<ul>";
+    for ($i = 1; $i <= 5; $i++) {
+        echo "<li>$i</li>";
+    }
+    echo "</ul>";
+    
+    /* TP 1
+    - Déclarer une variable $x = 1 et une variable $y = 835
+    - En utilisant la boucle "while" ajoutez successivement 1 à x
+      jusqu'à qu'il soit égale à y. Dans ce cas, affichez x et y
+    - Donnez une variante de cet boucle avec la boucle do... while
+    - Ecrivez une boucle qui affiche les multiples du nombre 7 inférieurs à 1000
+    - Utilisez la boucle for pour tester si le nombre 3457 est premier
+*/
+ /* 1er - Déclarer une variable $x = 1 et une variable $y = 835
+    - En utilisant la boucle "while" ajoutez successivement 1 à x
+      jusqu'à qu'il soit égale à y. Dans ce cas, affichez x et y */
+
+$x=1;
+$y=835;
+echo "<ul>";
+while ($x < $y) {
+    $x++;
+}
+echo "<li>$y</li>";
+echo "<li>$x</li>";
+echo "</ul>";
+/* 2eme - - Donnez une variante de cet boucle avec la boucle do... while */
+
+$x = 1;
+$y=835;
+echo "<ul>";
+do {
+    $x++;
+} while ($x<$y);
+echo "<li>$y</li>";
+echo "<li>$x</li>";
+echo "</ul>";
+
+/* 3eme-Ecrivez une boucle qui affiche les multiples du nombre 7 inférieurs à 1000 */
+
+$x = 7;
+
+echo "<ul>";
+for($i=7 ; $i<1000;$i++)
+{
+    if($i % 7 == 0)
+    {
+    echo "<li>$i</li>";}
+}
+echo "</ul>";
+
+
+// 4eme-Utilisez la boucle for pour tester si le nombre 3457 est premier
+
+
+for ($i = 2; $i < 3457; $i++) {
+    if (3457 % $i == 0)
+    {
+        echo " Le nombre 3457 n'est pas premier";
+        break;
+    }  
+    else if ($i==3456)
+    {
+        echo "Le nombre 3457 est premier";
+    }
+}
+
+
+?>
+
+</body> 
 </html>
